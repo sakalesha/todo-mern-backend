6994c08ce49ea6e61adf1c3131b5501f818f2549
sakalesha <149064711+sakalesha@users.noreply.github.com>
2025-08-22 23:46:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ mongoose.connect(process.env.MONGODB_URI, {
 .catch(err => console.error('MongoDB connection error:', err));
 
 // User Authentication
-require('dotenv').config();
 const authRouter = require('./routes/auth');
 const jwt = require('jsonwebtoken');
 
